ddf713f9ce93dd73fd4b1aa1edfe5c2e3249d0be
ram <ram@localhost>
2018-03-21 23:31:08 +0530

    first commit

---
+++ multimodule-project/webapp1/src/main/java/com/website/multimodule/App.java
@@ -0,0 +1,13 @@
+package com.website.multimodule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
